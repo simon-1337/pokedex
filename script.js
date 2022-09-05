ce9af4f451340c718059e3a8f3456fe43bba3215
simon-1337 <simon.besenbaeck@gmail.com>
2022-09-05 19:22:22 +0200

    load first Pokemons, show Names and Pictures

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-let currentPokemon
+let currentPokemon;
+let loadedPokemon = [];
+
+
+async function init() {
+    loadPokemon();
+ }
+
 
 async function loadPokemon() {
-    let url =  'https://pokeapi.co/api/v2/pokemon/charmander';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-
-    console.log(currentPokemon);
-    renderPokemonInfo();
+    let start = loadedPokemon.length + 1;
+    let end = start + 14;
+    for (let i = start; i < end; i++) {
+        let url =  `https://pokeapi.co/api/v2/pokemon/${i}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        loadedPokemon.push(currentPokemon);
+    }
+    renderPokedex(start-1);
 }
+
+
+function renderPokedex(renderStart) {
+    pokedexContainer = document.getElementById('pokedex');
+    for (let i = renderStart; i < loadedPokemon.length; i++) {
+        pokedexContainer.innerHTML += generatePokedexHTML(i);
+    } 
+}
+
+
+function generatePokedexHTML(i) {
+    return /*html*/ `
+        <div onclick="showDetails(${i})">
+            <h2>${loadedPokemon[i]['name']}</h2>
+            <img src=${loadedPokemon[i]['sprites']['other']['home']['front_default']}>
+        </div>
+    `;
+}
+
 
 function renderPokemonInfo() {
     let name = currentPokemon['name']
@@ -15,6 +44,7 @@ function renderPokemonInfo() {
     document.getElementById('pokemonName').innerHTML = name;
     document.getElementById('pokemonImg').src = currentPokemon['sprites']['other']['home']['front_default'];
 }
+
 
 function capitalizeFirstLetter(name) {
     return name.charAt(0).toUpperCase() + name.slice(1);
